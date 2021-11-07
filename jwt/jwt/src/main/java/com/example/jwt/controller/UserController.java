16b68bd9a01d1ded5a5a897c52348c62dcad1101
eunhee <dmsgml9903@gmail.com>
2021-11-07 22:56:34 +0900

    완료

---
+++ jwt/jwt/src/main/java/com/example/jwt/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.jwt.controller;
+
+import com.example.jwt.dto.UserDto;
+import com.example.jwt.entity.User;
+import com.example.jwt.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/test-redirect")
+    public void testRedirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/api/user");
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(
+            @Valid @RequestBody UserDto userDto
+    ) {
+        return ResponseEntity.ok(userService.signup(userDto));
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')") // User와 ADMIN 권한 모두 호출 가능하도록
+    public ResponseEntity<User> getMyUserInfo(HttpServletRequest request) {
+        return ResponseEntity.ok(userService.getMyUserWithAuthorities().get());
+    }
+
+    @GetMapping("/user/{username}")
+    @PreAuthorize("hasAnyRole('ADMIN')") // ADMIN권한만
+    public ResponseEntity<User> getUserInfo(@PathVariable String username) {
+        return ResponseEntity.ok(userService.getUserWithAuthorities(username).get());
+    }
+}
---
+++ jwt/jwt/src/main/java/com/example/jwt/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.jwt.repository;
+import com.example.jwt.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+// repository : JPA레파지토리를 상속함
+public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = "authorities")
+    // username을 기준으로 User정보를 가져올 때 권한 정보도 같이 가져오게 된다.
+    Optional<User> findOneWithAuthoritiesByUsername(String username);
+}
